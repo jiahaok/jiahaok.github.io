ce4ede02c4ab68b25e44e3988b464a692d48355e
xeonjake <jiahaokuang7066@gmail.com>
2016-02-15 23:36:06 -0800

    version 1.0 completed, seo completed

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
-
 var express = require('express');
 var app = express();
 var path = require('path');
 var port = process.env.PORT || 80;
+var date = new Date();
 
 app.use(express.static(path.join(__dirname+"/bower_components")));
 app.use(express.static(path.join(__dirname+"/static")));
 
-app.get('/', function(request, response){
+app.get('/*', function(request, response){
   response.sendFile(path.join(__dirname+'/index.html'));
-  console.log("Get request To "+ request.url +" From "+ request.ip);
+  console.log(date.toString()+": Get request To "+ request.url +" From "+ request.ip);
 });
 
 app.listen(port);
